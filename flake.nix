38fb6e37635fd5bac9e5031f4751208d6e7ddd15
Adrian Hesketh <adrianhesketh@hushmail.com>
2024-11-04 17:48:22 +0000

    feat: upgrade nix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
   inputs = {
+    nix.url = "github:nixos/nix/2.24.10";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { nixpkgs, nix, ... }:
     let
       adrianSSHKey = ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4ZYYVVw4dsNtzOnBCTXbjuRqOowMOvP3zetYXeE5i+2Strt1K4vAw37nrIwx3JsSghxq1Qrg9ra0aFJbwtaN3119RR0TaHpatc6TJCtwuXwkIGtwHf0/HTt6AH8WOt7RFCNbH3FuoJ1oOqx6LZOqdhUjAlWRDv6XH9aTnsEk8zf+1m30SQrG8Vcclj1CTFMAa+o6BgGdHoextOhGMlTx8ESAlgIXCo+dIVjANE2qbfAg0XL0+BpwlRDJt5OcgzrILXZ1jSIYRW4eg/JBcDW/WqorEummxhB26Y6R0jeswRF3DOQhU2fAhbsCWdairLam42rFGlKfWyTbgjRXl/BNR'';
       rootSSHKey = ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjt4N/HZ+dOEJ62OunmT0ZF2SqsT96iUdfSi6ZP83wt root@adrian.local'';
@@ -15,7 +16,14 @@
       ];
       forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
         inherit system;
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (self: super: {
+              nix = nix.packages.${system}.nix;
+            })
+          ];
+        };
       });
       devTools = { system, pkgs }: [
         pkgs.minio-client
