705d5cc1e4a52bbcdfc2751831130c46af25c77f
Adrian Hesketh <adrianhesketh@hushmail.com>
2024-11-04 20:19:21 +0000

    feat: use flake for nix-serve-ng

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
   inputs = {
     nix.url = "github:nixos/nix/2.24.10";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nix-serve-ng.url = "github:aristanetworks/nix-serve-ng";
   };
 
-  outputs = { nixpkgs, nix, ... }@inputs:
+  outputs = { nixpkgs, nix, nix-serve-ng, ... }@inputs:
     let
       adrianSSHKey = ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4ZYYVVw4dsNtzOnBCTXbjuRqOowMOvP3zetYXeE5i+2Strt1K4vAw37nrIwx3JsSghxq1Qrg9ra0aFJbwtaN3119RR0TaHpatc6TJCtwuXwkIGtwHf0/HTt6AH8WOt7RFCNbH3FuoJ1oOqx6LZOqdhUjAlWRDv6XH9aTnsEk8zf+1m30SQrG8Vcclj1CTFMAa+o6BgGdHoextOhGMlTx8ESAlgIXCo+dIVjANE2qbfAg0XL0+BpwlRDJt5OcgzrILXZ1jSIYRW4eg/JBcDW/WqorEummxhB26Y6R0jeswRF3DOQhU2fAhbsCWdairLam42rFGlKfWyTbgjRXl/BNR'';
       rootSSHKey = ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjt4N/HZ+dOEJ62OunmT0ZF2SqsT96iUdfSi6ZP83wt root@adrian.local'';
@@ -38,6 +39,7 @@
             rootSSHKey = rootSSHKey;
           };
           modules = [
+            nix-serve-ng.nixosModules.default
             ./systems/hetzner/builder/config.nix
             {
               imports = [ "${nixpkgs}/nixos/modules/profiles/hardened.nix" ];
