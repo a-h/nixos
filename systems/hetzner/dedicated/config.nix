a3ce91934b8ee7036ce5cd80bb660db75cfac660
Adrian Hesketh <adrianhesketh@hushmail.com>
2024-11-04 18:19:37 +0000

    feat: try and update nix

--- systems/hetzner/dedicated/config.nix
+++ systems/hetzner/dedicated/config.nix
@@ -44,9 +44,9 @@
 { system, inputs, pkgs, config, lib, adrianSSHKey, rootSSHKey, ... }:
 {
   nixpkgs = {
-    overlays = ../../../overlays/default.nix {
+    overlays = (../../../overlays/default.nix {
       inherit system inputs;
-    }.overlays;
+    }).overlays;
     config = {
       allowUnfree = true;
     };
